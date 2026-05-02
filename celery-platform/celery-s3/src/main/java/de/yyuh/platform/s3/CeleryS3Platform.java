7186faf45e0a3845b607f94f63f4447f7fbdfe03
yyuh <yyuh@femboys-are-hot.xyz>
2026-05-02 17:52:09 +0200

    fix: forgot to register s3 file storage in platform

--- celery-platform/celery-s3/src/main/java/de/yyuh/platform/s3/CeleryS3Platform.java
+++ celery-platform/celery-s3/src/main/java/de/yyuh/platform/s3/CeleryS3Platform.java
@@ -3,16 +3,20 @@ package de.yyuh.platform.s3;
 import de.yyuh.celery.api.CeleryDatabaseType;
 import de.yyuh.celery.api.CeleryPlatformType;
 import de.yyuh.celery.api.platform.AbstractCeleryPlatform;
+import de.yyuh.celery.api.provider.IFileStorageProvider;
 import de.yyuh.celery.api.provider.IProvider;
+import de.yyuh.platform.s3.provider.S3FileStorageProvider;
 
 public final class CeleryS3Platform extends AbstractCeleryPlatform {
 
   public CeleryS3Platform() {
     super("s3", CeleryDatabaseType.S3, CeleryPlatformType.FILE_STORAGE);
+
+    this.registerProvider(IFileStorageProvider.class, new S3FileStorageProvider());
   }
 
   @Override
   public IProvider defaultProvider() {
-    return null;
+    return this.provider(IFileStorageProvider.class).orElseThrow();
   }
 }
